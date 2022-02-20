cbc0d9ad5443178dd811d532b028a50830afee6b
Anowar <1hafizajr@gmail.com>
2022-02-20 12:49:02 +0600

    added chart

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,7 +14,7 @@
 <body class="sb-nav-fixed">
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
 <!-- Navbar Brand-->
-<a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
+<a class="navbar-brand ps-3" href="#">Admin Area </a>
 <!-- Sidebar Toggle-->
 <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     
@@ -85,10 +85,9 @@
 </div>
 <script src="{{asset('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js')}}"></script>
 <script src="{{asset('js/scripts.js')}}"></script>
-<script src="{{asset('https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js')}}"></script>
-
-<script src="{{asset('assets/demo/chart-bar-demo.js')}}"></script>
 <script src="{{asset('https://cdn.jsdelivr.net/npm/simple-datatables@latest')}}"></script>
 <script src="{{asset('js/datatables-simple-demo.js')}}"></script>
+
+
 </body>
 </html>
